03eda2cb8907446b130236db2dc345d3255d4b47
Aaqib Hussain <165165468+ahussain119@users.noreply.github.com>
2024-05-31 03:47:43 -0400

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -1,7 +1,7 @@
 # Purpose: This file is used to configure the remote backend for the terraform state file.  
 terraform {
     backend "s3" {
-        bucket = "aws-tier-2-remote-storage-aaqib"
+        bucket = "BucketName"
         key    = "key/terraform.tfstate"
         region = "us-west-2"
     }
